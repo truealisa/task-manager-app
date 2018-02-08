960cc125360ed67d9feeb68c10bfd7b309b08e09
truealisa <truealisa@gmail.com>
2018-02-08 02:08:16 +0200

    Add Tasks API

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,46 @@
 class TasksController < ApplicationController
+  before_action :set_project
+  before_action :set_project_task, only: [:show, :update, :destroy]
+
+  # GET /projects/:project_id/tasks
+  def index
+    json_response(@project.tasks)
+  end
+
+  # GET /projects/:project_id/tasks/:id
+  def show
+    json_response(@task)
+  end
+
+  # POST /projects/:project_id/tasks
+  def create
+    @project.tasks.create!(task_params)
+    json_response(@project, :created)
+  end
+
+  # PUT /projects/:project_id/tasks/:id
+  def update
+    @task.update(task_params)
+    head :no_content
+  end
+
+  # DELETE /projects/:project_id/tasks/:id
+  def destroy
+    @task.destroy
+    head :no_content
+  end
+
+  private
+
+  def task_params
+    params.permit(:name, :status)
+  end
+
+  def set_project
+    @project = Project.find(params[:project_id])
+  end
+
+  def set_project_task
+    @task = @project.tasks.find_by!(id: params[:id]) if @project
+  end
 end
---
+++ spec/requests/tasks_spec.rb
@@ -0,0 +1,127 @@
+require 'rails_helper'
+
+RSpec.describe 'Tasks API' do
+  # Initialize the test data
+  let!(:project) { create(:project) }
+  let!(:tasks) { create_list(:task, 20, project_id: project.id) }
+  let(:project_id) { project.id }
+  let(:id) { tasks.first.id }
+
+  # Test suite for GET /projects/:project_id/tasks
+  describe 'GET /projects/:project_id/tasks' do
+    before { get "/projects/#{project_id}/tasks" }
+
+    context 'when project exists' do
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'returns all project tasks' do
+        expect(json.size).to eq(20)
+      end
+    end
+
+    context 'when project does not exist' do
+      let(:project_id) { 0 }
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find Project/)
+      end
+    end
+  end
+
+  # Test suite for GET /projects/:project_id/tasks/:id
+  describe 'GET /projects/:project_id/tasks/:id' do
+    before { get "/projects/#{project_id}/tasks/#{id}" }
+
+    context 'when project task exists' do
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'returns the task' do
+        expect(json['id']).to eq(id)
+      end
+    end
+
+    context 'when project task does not exist' do
+      let(:id) { 0 }
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find Task/)
+      end
+    end
+  end
+
+  # Test suite for PUT /projects/:project_id/tasks
+  describe 'POST /projects/:project_id/tasks' do
+    let(:valid_attributes) { { name: 'Visit Narnia', done: false } }
+
+    context 'when request attributes are valid' do
+      before { post "/projects/#{project_id}/tasks", params: valid_attributes }
+
+      it 'returns status code 201' do
+        expect(response).to have_http_status(201)
+      end
+    end
+
+    context 'when an invalid request' do
+      before { post "/projects/#{project_id}/tasks", params: {} }
+
+      it 'returns status code 422' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'returns a failure message' do
+        expect(response.body).to match(/Validation failed: Name can't be blank/)
+      end
+    end
+  end
+
+  # Test suite for PUT /projects/:project_id/tasks/:id
+  describe 'PUT /projects/:project_id/tasks/:id' do
+    let(:valid_attributes) { { name: 'Mozart' } }
+
+    before { put "/projects/#{project_id}/tasks/#{id}", params: valid_attributes }
+
+    context 'when task exists' do
+      it 'returns status code 204' do
+        expect(response).to have_http_status(204)
+      end
+
+      it 'updates the task' do
+        updated_task = Task.find(id)
+        expect(updated_task.name).to match(/Mozart/)
+      end
+    end
+
+    context 'when the task does not exist' do
+      let(:id) { 0 }
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find Task/)
+      end
+    end
+  end
+
+  # Test suite for DELETE /projects/:id
+  describe 'DELETE /projects/:id' do
+    before { delete "/projects/#{project_id}/tasks/#{id}" }
+
+    it 'returns status code 204' do
+      expect(response).to have_http_status(204)
+    end
+  end
+end
