ea1f5ee20abadab53e6588f80a049dcd46f5cfa7
truealisa <truealisa@gmail.com>
2018-02-13 18:11:53 +0200

    Install Bootstrap and jQuery

--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -1,6 +1,9 @@
+// import Vue and components
 import Vue from 'vue'
 import router from '../router'
 import App from '../components/app.vue'
+// import css
+import "./application.css";
 
 document.addEventListener('DOMContentLoaded', () => {
   const el = document.body.appendChild(document.createElement('application'))
